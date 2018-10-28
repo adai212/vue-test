76b01e921a53979b7f54ce249cb727746802e015
kongmeng <987999409@qq.com>
2018-10-29 00:06:09 +0800

    add i18n

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <label for="locale">locale</label>
+    <select v-model="locale">
+      <option>en</option>
+      <option>cn</option>
+    </select>
+    <p>message: {{ $t('hello') }}</p>
+    <!-- <router-view/> -->
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () { return { locale: 'cn' } },
+  watch: {
+    locale (val) {
+      this.$i18n.locale = val;
+      this.$i18n._root.locale = val;
+    }
+  }
 }
 </script>
 
@@ -21,3 +33,13 @@ export default {
   margin-top: 60px;
 }
 </style>
+
+<i18n>
+{
+  "en": {
+  },
+  "cn": {
+    "hello": "你好,我的世界！"
+  }
+}
+</i18n>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueI18n from 'vue-i18n'
+
+Vue.use(VueI18n)
+const messages = {
+    en: require('./i18n/app-en.json'),
+    cn: require('./i18n/app-zh.json')
+}
+const i18n = new VueI18n({
+    locale: 'cn', // 语言标识
+    fallbackLocale: '',
+    messages
+})
 
 Vue.config.productionTip = false
 
@@ -10,6 +22,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  i18n,
   components: { App },
   template: '<App/>'
-})
+}).$mount('#app')
